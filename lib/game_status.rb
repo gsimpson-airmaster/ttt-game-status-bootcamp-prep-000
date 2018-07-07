6f5add21ed6c4c8216ad2dd0904eebd024ecd80e
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-07 00:54:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,30 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    if(board[win_combo[0]].upcase == "X" && board[win_combo[1]].upcase == "X" && board[win_combo[2]].upcase == "X" )
+      return win_combo
+    elsif (board[win_combo[0]].upcase == "O" && board[win_combo[1]].upcase == "O" && board[win_combo[2]].upcase == "O" )
+      return win_combo
+    end
+  end
+  return false
+end
+
+def full?(board)
+  if(!(board.includes))
+end
+
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [6,4,2],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+  ]
